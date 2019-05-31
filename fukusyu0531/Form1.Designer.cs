82e787677263a7abfa134ce94ff4f37b5f0c33e2
鎌田　拓臣 <yuuteayuu335dat@gmail.com>
2019-05-31 13:21:27 +0900

    Up

--- fukusyu0531/Form1.Designer.cs
+++ fukusyu0531/Form1.Designer.cs
@@ -29,11 +29,42 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- fukusyu0531/Form1.cs
+++ fukusyu0531/Form1.cs
@@ -12,9 +12,41 @@ namespace fukusyu0531
 {
     public partial class Form1 : Form
     {
+        int vx=5, vy=5;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left >= 255)
+            {
+                vx = -vx;
+            }
+
+            if (label1.Left <= 0)
+            {
+                vx = -vx;
+            }
+
+            if (label1.Top >= 255)
+            {
+                vy = -vy;
+            }
+
+            if (label1.Top <= 0)
+            {
+                vy = -vy;
+            }
         }
     }
 }
